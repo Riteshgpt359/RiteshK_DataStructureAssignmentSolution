a1ac101282e77c0a5d5e318896ca6395d77db9f8
Ritz <Ritz@DESKTOP-1JMCTAL>
2022-11-25 00:18:58 +0530

    First commit

---
+++ Graded Coding Assignment 2/src/com/transactionsbst/model/Node.java
@@ -0,0 +1,16 @@
+package com.transactionsbst.model;
+
+/*
+ * This class will help us create nodes for our BST
+ * */
+
+public class Node {
+	public int data;
+	public Node left, right;
+
+	public Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
